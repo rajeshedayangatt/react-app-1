3a626fcf40ca0af17a3c9a7399601e538e254447
Rajesh <rajeshedayangat@gmail.com>
2019-02-13 21:56:58 +0530

    contextapi

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,50 @@ import React, { Component } from "react";
 import "../assets/css/style.css";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.loginUser = this.loginUser.bind(this);
+    this.changeInput = this.changeInput.bind(this);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  loginUser(e) {
+    e.preventDefault();
+  }
+  changeInput(e) {
+    this.setState({
+      [e.target.name]: [e.target.value]
+    });
+  }
   render() {
     return (
       <div className="login_form">
-        <div className="form-group">
-          <label>Email</label>
-          <input type="email" name="email" placeholder="Enter Email Address" />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input type="password" name="password" placeholder="Enter Password" />
-        </div>
-        <button>Login</button>
+        <form onSubmit={this.loginUser}>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Email Address"
+              value={this.state.email}
+              onChange={this.changeInput}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+              value={this.state.password}
+              onChange={this.changeInput}
+            />
+          </div>
+          <button className="btn">Login</button>
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Login from "./components/Login";
+import AppProvider, { AppContext } from "./state/context";
 
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <Login />
-    </div>
+    <AppProvider>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <AppContext.Consumer>
+          {context => <button onClick={context.inc}>INC</button>}
+        </AppContext.Consumer>
+        <Login />
+
+        <AppContext.Consumer>
+          <h1>{context => context.number}</h1>
+        </AppContext.Consumer>
+      </div>
+    </AppProvider>
   );
 }
 
